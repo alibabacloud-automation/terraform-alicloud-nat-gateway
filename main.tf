d06fb779e1c7960de2a10c428bf2df0d7e8d1d2d
dianmuali <jince.jc@alibaba.com>
2018-11-23 21:41:26 +0800

    Add Alicloud Nat Gateway Module

--- main.tf
+++ main.tf
@@ -1,74 +1,51 @@
-// Instance_types data source for instance_type
-data "alicloud_instance_types" "1c2g" {
-  cpu_core_count = "${var.cpu_core_count}"
-  memory_size    = "${var.memory_size}"
+module "module_vpc" {
+  source = "alibaba/vpc/alicloud"
+
+  vpc_id   = "${var.vpc_id}"
+  vpc_name = "${var.name}"
+
+  vswitch_name  = "${var.name}"
+  vswitch_cidrs = "${var.vswitch_cidrs}"
 }
- data "alicloud_zones" "default" {
-  "available_instance_type" = "${data.alicloud_instance_types.1c2g.instance_types.0.id}"
-  "available_disk_category" = "${var.disk_category}"
+
+module "nat_gateway" {
+  source = "./modules/nat_gateway"
+
+  ###############################################################
+  #variables for nat gateway
+  ##############################################################
+  vpc_id = "${module.module_vpc.vpc_id}"
+
+  name          = "${var.name}"
+  specification = "${var.specification}"
+  description   = "${var.description}"
 }
- // If there is not specifying vpc_id, the module will launch a new vpc
-resource "alicloud_vpc" "default" {
-  count = "${var.vpc_id == "" ? 1 : 0}"
-  name = "${var.vpc_name == "" ? var.resource_group_name : var.vpc_name}"
-  cidr_block = "${var.vpc_cidr}"
+
+module "dnat_entry" {
+  source = "./modules/dnat_entry"
+
+  ###############################################################
+  #variables for dnat entry
+  ##############################################################
+  dnat_count = "${var.dnat_count}"
+
+  forward_table_id = "${var.forward_table_id}"
+  external_ips     = "${var.external_ips}"
+  external_ports   = "${var.external_ports}"
+  ip_protocols     = "${var.ip_protocols}"
+  internal_ips     = "${var.internal_ips}"
+  internal_ports   = "${var.internal_ports}"
 }
- resource "alicloud_vswitch" "default" {
-  vpc_id            = "${alicloud_vpc.default.id}"
-  cidr_block        = "${var.vswitch_cidr}"
-  availability_zone = "${data.alicloud_zones.default.zones.0.id}"
-}
- resource "alicloud_nat_gateway" "default" {
-  vpc_id = "${var.vpc_id == "" ? join("", alicloud_vpc.default.*.id) : var.vpc_id}"
-  name = "${var.resource_group_name}"
-}
- resource "alicloud_eip" "default" {
-  count     = 2
-  bandwidth = 5
-}
- resource "alicloud_eip_association" "default" {
-  count         = 2
-  allocation_id = "${element(alicloud_eip.default.*.id, count.index)}"
-  instance_id   = "${alicloud_nat_gateway.default.id}"
-}
- resource "alicloud_snat_entry" "default" {
-  snat_table_id     = "${alicloud_nat_gateway.default.snat_table_ids}"
-  source_vswitch_id = "${alicloud_vswitch.default.id}"
-  snat_ip           = "${alicloud_eip.default.0.ip_address}"
-}
- resource "alicloud_forward_entry" "default" {
-  forward_table_id = "${alicloud_nat_gateway.default.forward_table_ids}"
-  external_ip      = "${alicloud_eip.default.1.ip_address}"
-  external_port    = "${var.external_port}"
-  ip_protocol      = "${var.ip_protocol}"
-  internal_ip      = "${alicloud_instance.default.private_ip}"
-  internal_port    = "${var.internal_port}"
-}
- // Security Group Resource for Module
-resource "alicloud_security_group" "sg" {
-  vpc_id = "${var.vpc_id == "" ? join("", alicloud_vpc.default.*.id) : var.vpc_id}"
-  name   = "${var.group_name == "" ? var.resource_group_name : var.group_name}"
-}
- resource "alicloud_security_group_rule" "http-in" {
-  type              = "${var.type}"
-  ip_protocol       = "${var.ip_protocol}"
-  nic_type          = "${var.nic_type}"
-  policy            = "${var.policy}"
-  port_range        = "${var.port_range}"
-  priority          = "${var.priority}"
-  security_group_id = "${alicloud_security_group.sg.id}"
-  cidr_ip           = "${var.cidr_ip}"
-}
- resource "alicloud_instance" "default" {
-  # cn-beijing
-  availability_zone = "${data.alicloud_zones.default.zones.0.id}"
-  security_groups   = ["${alicloud_security_group.sg.id}"]
-   vswitch_id = "${alicloud_vswitch.default.id}"
-   # series III
-  instance_charge_type       = "${var.instance_charge_type}"
-  instance_type              = "${var.instance_type}"
-  internet_max_bandwidth_out = 0
-   system_disk_category = "cloud_efficiency"
-  image_id             = "${var.image_id}"
-  instance_name   = "${var.instance_name == "" ? var.resource_group_name : var.instance_name}"
+
+module "snat_entry" {
+  source = "./modules/snat_entry"
+
+  ###############################################################
+  #variables for db snat entry
+  ##############################################################
+  snat_count = "${var.snat_count}"
+
+  snat_table_id      = "${var.snat_table_id}"
+  source_vswitch_ids = "${split(",", module.module_vpc.vswitch_ids)}"
+  snat_ips           = "${var.snat_ips}"
 }
--- variables.tf
+++ variables.tf
@@ -1,97 +1,90 @@
-variable "resource_group_name" {
-  default = "tf-module-nat-gateway"
+##############################################################
+#variables for alicloud_nat_gateway
+##############################################################
+
+variable "name" {
+  default = "terraform-alicloud-nat-gateway"
 }
- variable "instance_name" {
-  description = "The instance name. Default to variable `resource_group_name`."
-  default     = ""
+
+variable "vpc_id" {
+  description = "An existing vpc id used to create a new nat gateway."
 }
- variable "instance_type" {
-  description = "The instance type used to launch one or more ecs instances."
-  default = "ecs.n4.small"
-}
- variable "image_id" {
-  description = "The image id used to launch one or more ecs instances."
-  default = "ubuntu_140405_64_40G_cloudinit_20161115.vhd"
-}
- variable "disk_category" {
-  description = "The data disk category used to launch one or more data disks."
-  default = "cloud_efficiency"
-}
- # VPC variables
-variable "vpc_name" {
-  description = "The vpc name used to create a new vpc when 'vpc_id' is not specified. Default to variable `resource_group_name`"
-  default     = ""
-}
- variable "vpc_id" {
-  description = "An existing vpc id used to create several vswitches and other resources."
-  default     = ""
-}
- variable "vpc_cidr" {
-  description = "The cidr block used to launch a new vpc when 'vpc_id' is not specified."
-  default = "10.1.0.0/21"
-}
- # VSwitch variables
-variable "vswitch_ids" {
-  description = "List existing vswitches id used to create ECS and RDS instance."
+
+variable "vswitch_cidrs" {
+  description = "List of cidr blocks used to launch several new vswitches."
   type        = "list"
-  default     = []
 }
- variable "vswitch_cidr" {
-  description = "The cidr block used to launch a new vswitch when 'vswitch_id' is not specified."
-  default = "10.1.1.0/24"
-}
- # Security Group variables
-variable "group_name" {
-  description = "The security group name. Default to variable `resource_group_name`."
+
+variable "nat_gateway_id" {
+  description = "The id of an existing nat gateway."
   default     = ""
 }
- # Instance typs variables
-variable "cpu_core_count" {
-  description = "CPU core count is used to fetch instance types."
-  default     = 1
+
+variable "specification" {
+  description = "The specification of nat gateway."
+  default     = "Small"
 }
- variable "memory_size" {
-  description = "Memory size used to fetch instance types."
-  default     = 2
+
+variable "description" {
+  description = "The description of nat gateway."
+  default     = "terraform-alicloud-nat-gateway"
 }
- variable "external_port" {
-  description = "The external port of dnat entry."
-  default     = "80"
+
+###############################################################
+#variables for alicloud_dnat_entry
+##############################################################
+
+variable "dnat_count" {
+  description = "Number of dnat entry."
 }
- variable "internal_port" {
-  description = "The internal port of dnat entry."
-  default     = "8080"
+
+variable "forward_table_id" {
+  description = "The forward table id of dnat entry."
 }
- # Security group rule variables
-variable "ip_protocol" {
-  description = "The ip protocol of dnat entry."
-  default     = "tcp"
+
+variable "external_ips" {
+  type        = "list"
+  description = "The external ip id of dnat entry. The length should be equal to dnat_count."
 }
- variable "type" {
-  description = "The type of the security group rule."
-  default     = "ingress"
+
+variable "internal_ips" {
+  type        = "list"
+  description = "The internal ip of dnat entry. The length should be equal to dnat_count."
 }
- variable "nic_type" {
-  description = "The nic type of the security group rule."
-  default     = "intranet"
+
+variable "external_ports" {
+  type        = "list"
+  description = "The external port of dnat entry. The length should be equal to dnat_count."
 }
- variable "policy" {
-  description = "The policy of the security group rule."
-  default     = "accept"
+
+variable "internal_ports" {
+  type        = "list"
+  description = "The internal port of dnat entry. The length should be equal to dnat_count."
 }
- variable "port_range" {
-  description = "The port range of the security group rule."
-  default     = "80/80"
+
+variable "ip_protocols" {
+  type        = "list"
+  description = "The ip protocols of dnat entry. The length should be equal to dnat_count."
 }
- variable "priority" {
-  description = "The priority of the security group rule."
-  default     = 1
+
+##############################################################
+#variables for alicloud_snat_entry
+##############################################################
+
+variable "snat_count" {
+  description = "Number of snat entry."
 }
- variable "cidr_ip" {
-  description = "The cidr ip of the security group rule."
-  default     = "0.0.0.0/0"
+
+variable "snat_table_id" {
+  description = "The snat table id of nat gateway."
 }
- variable "instance_charge_type" {
-  description = "The charge type of instance. Choices are 'PostPaid' and 'PrePaid'."
-  default     = "PostPaid"
-} 
+
+variable "source_vswitch_ids" {
+  type        = "list"
+  description = "The vswitch ids. The length should be equal to snat_count."
+}
+
+variable "snat_ips" {
+  type        = "list"
+  description = "The snat ips of the nat gateway. The length should be equal to snat_count."
+}
