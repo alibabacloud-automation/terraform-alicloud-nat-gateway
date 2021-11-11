b6bc5129e6440fabd5f82d6646b76727b318cf31
王超 <wc01169074@alibaba-inc.com>
2021-11-11 20:51:30 +0800

    Removes the provider setting and improves the Readme

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "alicloud" {
-  profile                 = var.profile != "" ? var.profile : null
-  shared_credentials_file = var.shared_credentials_file != "" ? var.shared_credentials_file : null
-  region                  = var.region != "" ? var.region : null
-  skip_region_validation  = var.skip_region_validation
-  configuration_source    = "terraform-alicloud-modules/nat-gateway"
-}
-
 resource "alicloud_nat_gateway" "this" {
   count         = var.use_existing_nat_gateway ? 0 : var.create ? 1 : 0
   vpc_id        = var.vpc_id
